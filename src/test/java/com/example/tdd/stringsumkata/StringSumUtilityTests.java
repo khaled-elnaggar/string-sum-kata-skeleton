0c3eccd9f603603068fafa166a9ec3f3e7f2176c
Khalid <dev_khalid@hotmail.com>
2021-10-27 15:30:22 +0200

    Implement test case #1

--- src/test/java/com/example/tdd/stringsumkata/StringSumUtilityTests.java
+++ src/test/java/com/example/tdd/stringsumkata/StringSumUtilityTests.java
@@ -1,7 +1,26 @@
 package com.example.tdd.stringsumkata;
 
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 class StringSumUtilityTests {
 
+    @Test
+    public void addTwoStringsThatAreRealNumbers(){
+         // Arrange
+         String firstNumber = "10";
+         String secondNumber = "150";
+
+         // Act
+         Long actualSum = StringSumUtility.addTwoNumbers(firstNumber, secondNumber);
+
+         // Assert
+        Long expectedSum = 160L;
+
+        assertEquals(expectedSum, actualSum);
+
+    }
 
 }
